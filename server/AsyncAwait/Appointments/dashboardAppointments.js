7d641b1285e56c0b7b29d72fe2fd3218a63d5ee5
alvinaleguiojo <alvin.aleguiojo@gmail.com>
2022-11-28 04:23:40 +0800

    fixing date

--- server/AsyncAwait/Appointments/dashboardAppointments.js
+++ server/AsyncAwait/Appointments/dashboardAppointments.js
@@ -1,13 +1,14 @@
 // import db connection
 const connection = require("../../db/connection");
+const moment = require("moment");
 
 // Get All Patients
 const DashboardAppointmentsPromise = () => {
-  const today = new Date();
-  today.setDate(today.getDate() + 30);
-  const date = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
+  // const today = new Date();
+  // today.setDate(today.getDate() + 30);
   // ✅ 31 Day added
-  console.log(date);
+  // console.log(date);
+  const date = moment().format("YYYY-MM-DD")
 
   const query = `SELECT Patients.PatientID, Patients.FirstName, Patients.LastName, Services.ServiceType, Appointments.AppointmentID, Appointments.Status, Services.Color FROM Patients INNER JOIN Appointments ON Patients.PatientID = Appointments.PatientID INNER JOIN Services ON Appointments.ServiceID = Services.ServiceID WHERE Schedule = '${date}'`;
   return new Promise((resolve, reject) => {
